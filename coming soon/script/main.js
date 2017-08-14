2811dfbfc16c9cb11f7f98b05fd0d6bf519b3bb0
Cole Hansen <cole_hansen@brown.edu>
2017-08-14 14:47:53 +0000

    Remove 'coming soon'
    No longer needed and archive will exist in version control

--- coming soon/script/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  // scroll settings
-  $('body').smoothScroll({
-    delegateSelector: 'nav a',
-    speed: 'auto',
-    autoCoefficient: 2.5,
-    easing: 'swing'
-  });
-});
